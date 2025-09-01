70234c8a50a03de29f882f97ed3ce823cc6b7c6b
anshukumar-mobius <anshukumar.p@mobiusdtaas.ai>
2025-09-01 11:03:38 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { Buffer } from 'buffer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window',
-    Buffer: Buffer,
-    process: 'globalThis.process',
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-      util: 'util',
-      stream: 'stream-browserify',
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-    include: ['jsonwebtoken', 'jws', 'util', 'stream-browserify', 'readable-stream'],
   },
 });
